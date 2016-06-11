bf6a490904ebb0d1dde50d9f77be092d5842f056
Robin Millette <robin@millette.info>
2016-06-11 18:42:16 -0400

    With vault file.

--- vault.js
+++ vault.js
@@ -1,11 +1,13 @@
 'use strict'
 
 export default (req, res) => {
-  const ret = req.headers
+  if (req.headers['x-forwarded-for'] !== '69.171.154.246') {
+    return 'oh well'
+  }
 
-  ret.auth = new Buffer(ret.authorization.split(' ')[1], 'base64').toString('ascii').split(':')[0]
-  delete ret.authorization
-
-  return ret 
+  const ret = require('vault-content.json')
+  ret.headers = req.headers
+  ret.auth = new Buffer(req.headers.authorization.split(' ')[1], 'base64').toString('ascii').split(':')[0]
+  delete ret.headers.authorization
+  return ret
 }
-
