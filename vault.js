e14ee77922965f4864872f95e95943e6933f7ee9
Robin Millette <robin@millette.info>
2016-06-14 07:51:06 -0400

    Use main from package.json.

--- vault.js
+++ vault.js
@@ -5,7 +5,7 @@ export default (req, res) => {
     return 'oh well'
   }
 
-  const ret = require('vault-content.json')
+  const ret = require('./vault-content.json')
   ret.headers = req.headers
   ret.auth = new Buffer(req.headers.authorization.split(' ')[1], 'base64').toString('ascii').split(':')[0]
   delete ret.headers.authorization
